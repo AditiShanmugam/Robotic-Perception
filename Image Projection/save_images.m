22697b1c3f82a2d972a2aae5290130554a3af25a
Aditi Shanmugam <sa663125@gmail.com>
2021-07-27 21:59:47 +0530

    Create save_images.m

---
+++ Image Projection/save_images.m
@@ -0,0 +1,17 @@
+function [] = save_images( images, im_name)
+% save_images saves the set of images in images to png files with prefix
+% im_name in folder warped_images
+% Inputs:
+%     images - Nx1 cell of images to be written to file
+%     im_name - string for prefix of file name
+if nargin==1
+    im_name = 'ar_img';
+end
+num_ima = length(images);
+
+for i=1:num_ima
+    imwrite(images{i}, sprintf('data/%s%03d.png',im_name,i));
+end
+
+end
+
